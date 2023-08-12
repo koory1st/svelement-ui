285003c12442917bf870004fbc4e5724a1d5b8ba
koory1st <32436334@qq.com>
2023-08-12 18:37:24 +0800

    fix(docs): use absolute url

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <header>
   SvelementUI
   <nav>
-    <a href="/component">Component</a>
+    <a href="{PUBLIC_BASE_PATH}/component">Component</a>
   </nav>
 </header>
 <slot/>
+
+<script lang="ts">
+import { PUBLIC_BASE_PATH } from '$env/static/public';
+</script>
