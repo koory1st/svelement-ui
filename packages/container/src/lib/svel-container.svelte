ae15cfc0e574f970f4804118b011c85cc379e9f6
koory1st <32436334@qq.com>
2023-08-12 20:24:13 +0800

    fix(container): change class name

--- packages/container/src/lib/svel-container.svelte
+++ packages/container/src/lib/svel-container.svelte
@@ -2,7 +2,7 @@
 	import a2s from '@svelement-ui/util-array-2-class-string';
 
   export let direction: 'horizontal' | 'vertical' = 'horizontal'
-  $: classString = a2s(['seu-container', ['is-vertical', direction === 'vertical']])
+  $: classString = a2s(['svel-container', ['is-vertical', direction === 'vertical']])
 </script>
 
 <section class={classString}>
