83dccd62fd913003ddfcf9fd52e813262e3691d3
koory1st <32436334@qq.com>
2023-08-13 12:05:07 +0800

    fix(docs): add container

--- docs/src/routes/component/+layout.svelte
+++ docs/src/routes/component/+layout.svelte
@@ -1,2 +1,23 @@
-component
-<slot/>
+<SvelContainer>
+  <SvelAside>
+    <div>Basic 基础组件</div>
+    <div>
+      <ul>
+        <li><a href="{PUBLIC_BASE_PATH}/component/button">Button 按钮</a></li>
+        <li><a href="{PUBLIC_BASE_PATH}/component/container">Container 布局容器</a></li>
+      </ul>
+    </div>
+  </SvelAside>
+
+  <SvelMain>
+    <slot/>
+  </SvelMain>
+</SvelContainer>
+
+
+<script lang="ts">
+import { PUBLIC_BASE_PATH } from '$env/static/public';
+import SvelContainer from '@svelement-ui/container';
+import SvelAside from '@svelement-ui/aside';
+import SvelMain from '@svelement-ui/main';
+</script>
