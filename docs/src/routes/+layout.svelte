66425843226da69db8805a8b0019e07b1d4ad4f0
levy <levy@mxlinux>
2023-08-15 20:12:21 +0800

    fix(docs): add row and col

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -1,27 +1,37 @@
+<script lang="ts">
+  import { PUBLIC_BASE_PATH } from '$env/static/public'
+  import './layout.scss'
+  import '@svelement-ui/container/dist/style.css'
+  import '@svelement-ui/header/dist/style.css'
+  import '@svelement-ui/footer/dist/style.css'
+  import '@svelement-ui/main/dist/style.css'
+  import '@svelement-ui/row/dist/style.css'
+  import '@svelement-ui/col/dist/style.css'
+  import SvelContainer from '@svelement-ui/container'
+  import SvelHeader from '@svelement-ui/header'
+  import SvelMain from '@svelement-ui/main'
+  import SvelFooter from '@svelement-ui/footer'
+  import SvelRow from '@svelement-ui/row'
+  import SvelCol from '@svelement-ui/col'
+</script>
+
 <SvelContainer direction="vertical">
-  <SvelHeader>
-    SvelementUI header
-    <nav>
-      <a href="{PUBLIC_BASE_PATH}component">Component</a>
-    </nav>
+  <SvelHeader class="header">
+    <SvelRow class="header-row">
+      <SvelCol span="6">SvelementUI</SvelCol>
+      <SvelCol span="18">
+        <nav>
+          <a href="{PUBLIC_BASE_PATH}component">Component</a>
+        </nav>
+      </SvelCol>
+    </SvelRow>
   </SvelHeader>
 
   <SvelMain>
-    <slot/>
+    <slot />
   </SvelMain>
-  <SvelFooter>
-    SvelementUI footer
-  </SvelFooter>
+  <SvelFooter>SvelementUI footer</SvelFooter>
 </SvelContainer>
 
-<script lang="ts">
-import { PUBLIC_BASE_PATH } from '$env/static/public';
-import "@svelement-ui/container/dist/style.css"
-import "@svelement-ui/header/dist/style.css"
-import "@svelement-ui/footer/dist/style.css"
-import "@svelement-ui/main/dist/style.css"
-import SvelContainer from '@svelement-ui/container';
-import SvelHeader from '@svelement-ui/header';
-import SvelMain from '@svelement-ui/main';
-import SvelFooter from '@svelement-ui/footer';
-</script>
+<style>
+</style>
--- docs/src/routes/component/container/+page.svelte
+++ docs/src/routes/component/container/+page.svelte
@@ -1,5 +1,3 @@
-
-
 <script lang="ts">
 import "@svelement-ui/container/dist/style.css"
 import SvelContainer from "@svelement-ui/container";
