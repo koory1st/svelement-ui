19d2e45d0422ebe24bdc794789ef62cd5268d32e
koory1st <32436334@qq.com>
2023-09-09 20:14:54 +0800

    fix(icon): add icons

--- packages/icon/src/lib/edit.svelte
+++ packages/icon/src/lib/edit.svelte
@@ -1,4 +1,4 @@
-<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
   <path
     fill="currentColor"
     d="M832 512a32 32 0 1 1 64 0v352a32 32 0 0 1-32 32H160a32 32 0 0 1-32-32V160a32 32 0 0 1 32-32h352a32 32 0 0 1 0 64H192v640h640V512z"
--- packages/icon/src/lib/index.ts
+++ packages/icon/src/lib/index.ts
@@ -1,2 +1,3 @@
-export { default as Loading } from './loading.svelte';
+export { default as AddLocation } from './add-location.svelte';
+export { default as Aim } from './aim.svelte';
 export { default as SvelIcon } from './icon.svelte';
