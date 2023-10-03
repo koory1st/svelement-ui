2739a2b7517dac0008101ff9dd0021bcf4448bd7
koory1st <32436334@qq.com>
2023-10-03 19:00:29 +0800

    fix(docs): layout

--- docs/src/lib/i18nZhCn.ts
+++ docs/src/lib/i18nZhCn.ts
@@ -30,6 +30,22 @@ const langDict: { [key: string]: string } = {
   cico01010: 'Icon 图标',
 
   clay01010: 'Layout 布局',
+  clay01020: '通过基础的 24 分栏，迅速简便地创建布局。',
+  clay02010: '基础布局',
+  clay02020: '使用列创建基础网格布局。',
+  clay02030: '通过 row 和 col 组件，并通过 col 组件的 span 属性我们就可以自由地组合布局。',
+  clay03010: '分栏间隔',
+  clay03020: '支持列间距。',
+  clay03030: '行提供 gutter 属性来指定列之间的间距，其默认值为0。',
+  clay04010: '混合布局',
+  clay04020: '通过基础的 1/24 分栏任意扩展组合形成较为复杂的混合布局。',
+  clay05010: '列偏移',
+  clay05020: '您可以指定列偏移量。',
+  clay05030: '通过制定 col 组件的 offset 属性可以指定分栏偏移的栏数。',
+  clay06010: '对齐方式',
+  clay06020: '默认使用 flex 布局来对分栏进行灵活的对齐。',
+  clay06030:
+    '您可以通过justify 属性来定义子元素的排版方式，其取值为start、center、end、space-between、space-around或space-evenly。',
 
   odiv01010: 'Divider 分割线',
   odiv01020: '区隔内容的分割线。',
--- docs/src/routes/component/layout/+page.svelte
+++ docs/src/routes/component/layout/+page.svelte
@@ -1,2 +1,259 @@
 <script lang="ts">
+  import { SvelRow, SvelCol } from '@svelement-ui/all';
+  import type GetText from '$lib/i18n';
+  import { getContext } from 'svelte';
+  import Example from '$lib/example.svelte';
+  let lang: GetText = getContext('lang');
 </script>
+
+<h1>{lang('clay01010')}</h1>
+<p>{lang('clay01020')}</p>
+
+<h2>{lang('clay02010')}</h2>
+<p>{lang('clay02020')}</p>
+<p>{lang('clay02030')}</p>
+
+<Example
+  code={`
+<SvelRow class="mb-4">
+  <SvelCol span={24}><div class="grid-content ep-bg-purple-dark" /></SvelCol>
+</SvelRow>
+
+<SvelRow class="mb-4">
+  <SvelCol span={12}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={12}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+</SvelRow>
+
+<SvelRow class="mb-4">
+  <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={8}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+
+<SvelRow class="mb-4">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+</SvelRow>
+<SvelRow>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+</SvelRow>`}
+>
+  <SvelRow class="mb-4">
+    <SvelCol span={24}><div class="grid-content ep-bg-purple-dark" /></SvelCol>
+  </SvelRow>
+
+  <SvelRow class="mb-4">
+    <SvelCol span={12}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={12}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  </SvelRow>
+
+  <SvelRow class="mb-4">
+    <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={8}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+
+  <SvelRow class="mb-4">
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  </SvelRow>
+  <SvelRow>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  </SvelRow>
+</Example>
+
+<h2>{lang('clay03010')}</h2>
+<p>{lang('clay03020')}</p>
+<p>{lang('clay03030')}</p>
+
+<Example
+  code={`
+<SvelRow gutter={40} class="mb-4">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+</SvelRow>`}
+>
+  <SvelRow gutter={40}>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  </SvelRow>
+</Example>
+
+<h2>{lang('clay04010')}</h2>
+<p>{lang('clay04020')}</p>
+<Example
+  code={`
+<SvelRow gutter={20} class="mb-4">
+  <SvelCol span={16}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow gutter={20} class="mb-4">
+  <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow gutter={20} class="mb-4">
+  <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={16}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>`}
+>
+  <SvelRow gutter={20} class="mb-4">
+    <SvelCol span={16}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow gutter={20} class="mb-4">
+    <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={8}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow gutter={20}>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={16}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={4}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+</Example>
+
+<h2>{lang('clay05010')}</h2>
+<p>{lang('clay05020')}</p>
+<p>{lang('clay05030')}</p>
+<Example
+  code={`
+<SvelRow gutter={20} class="mb-4">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6} offset={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow gutter={20} class="mb-4">
+  <SvelCol span={6} offset={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6} offset={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow gutter={20}>
+  <SvelCol span={12} offset={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>`}
+>
+  <SvelRow gutter={20} class="mb-4">
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6} offset={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow gutter={20} class="mb-4">
+    <SvelCol span={6} offset={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6} offset={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow gutter={20}>
+    <SvelCol span={12} offset={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+</Example>
+
+<h2>{lang('clay06010')}</h2>
+<p>{lang('clay06020')}</p>
+<p>{lang('clay06030')}</p>
+<Example
+  code={`
+<SvelRow class="mb-4 row-bg">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow class="mb-4 row-bg" justify="center">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow class="mb-4 row-bg" justify="end">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow class="mb-4 row-bg" justify="space-between">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow class="mb-4 row-bg" justify="space-around">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>
+<SvelRow class="row-bg" justify="space-evenly">
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+  <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+</SvelRow>`}
+>
+  <SvelRow class="mb-4 row-bg">
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow class="mb-4 row-bg" justify="center">
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow class="mb-4 row-bg" justify="end">
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow class="mb-4 row-bg" justify="space-between">
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow class="mb-4 row-bg" justify="space-around">
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+  <SvelRow class="row-bg" justify="space-evenly">
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple-light" /></SvelCol>
+    <SvelCol span={6}><div class="grid-content ep-bg-purple" /></SvelCol>
+  </SvelRow>
+</Example>
+
+<style>
+  :global(:root) {
+    --ep-c-bg-row: #f9fafc;
+    --ep-c-bg-purple: #d3dce6;
+    --ep-c-bg-purple-dark: #99a9bf;
+    --ep-c-bg-purple-light: #e5e9f2;
+  }
+  :global(.grid-content) {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .ep-bg-purple-dark {
+    background: var(--ep-c-bg-purple-dark);
+  }
+  .ep-bg-purple {
+    background: var(--ep-c-bg-purple);
+  }
+  .ep-bg-purple-light {
+    background: var(--ep-c-bg-purple-light);
+  }
+  :global(.row-bg) {
+    padding: 10px 0;
+    background-color: var(--ep-c-bg-row);
+  }
+</style>
