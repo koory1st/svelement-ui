43809d1890e4912f3b9a55fc10df7eb4035cbd5e
koory1st <32436334@qq.com>
2023-10-04 19:21:24 +0800

    refactor(main): use svelte build

--- packages/main/src/lib/index.ts
+++ packages/main/src/lib/index.ts
@@ -1,2 +1,6 @@
 import '@svelement-ui/theme-chalk/src/base.scss';
 import '@svelement-ui/theme-chalk/src/main.scss';
+
+import SvelMain from './main.svelte';
+
+export default SvelMain;
---
+++ packages/main/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import SvelLink from '$lib/index.js';
+</script>
+
+<SvelLink>aaaa</SvelLink>
