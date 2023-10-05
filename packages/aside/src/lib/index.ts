9e35b19a9f16ce8cfb922d5af21dad307c68ed51
koory1st <32436334@qq.com>
2023-10-05 14:40:05 +0800

    refactor(aside): use svelte build

--- packages/aside/src/lib/index.ts
+++ packages/aside/src/lib/index.ts
@@ -1,2 +1,6 @@
 import '@svelement-ui/theme-chalk/src/base.scss';
 import '@svelement-ui/theme-chalk/src/aside.scss';
+
+import SvelAside from './aside.svelte';
+
+export default SvelAside;
--- packages/aside/vite.config.ts
+++ packages/aside/vite.config.ts
@@ -1,12 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/style.ts'),
-      name: 'index',
-      fileName: 'index',
-    },
-  },
+  plugins: [sveltekit()],
 });
