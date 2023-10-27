0fcabc7c96b582cd7bb8e759c19d12dc55a3b180
koory1st <32436334@qq.com>
2023-10-27 19:50:17 +0800

    fix(docs): add scrollbar content

--- docs/src/lib/i18n/zh/scrollbar.js
+++ docs/src/lib/i18n/zh/scrollbar.js
@@ -3,4 +3,6 @@ export default {
   cscrollbar01011: '用于替换浏览器原生滚动条。',
   cscrollbar02010: '基础用法',
   cscrollbar02011: '通过 height 属性设置滚动条高度，若不设置则根据父容器高度自适应。',
+  cscrollbar03010: '横向滚动',
+  cscrollbar03011: '当元素宽度大于滚动条宽度时，会显示横向滚动条。',
 };
--- docs/src/routes/component/scrollbar/+page.svelte
+++ docs/src/routes/component/scrollbar/+page.svelte
@@ -9,6 +9,11 @@
   for (let i = 0; i < 10; i++) {
     list.push(i);
   }
+
+  let list2 = [];
+  for (let i = 0; i < 50; i++) {
+    list2.push(i);
+  }
 </script>
 
 <h1>{$langFn('cscrollbar01010')}</h1>
@@ -39,6 +44,33 @@
   </SvelScrollbar>
 </Example>
 
+<h2>{$langFn('cscrollbar03010')}</h2>
+<p>{$langFn('cscrollbar03011')}</p>
+
+<Example
+  code={`
+<script>
+  let list = [];
+  for (let i = 0; i < 10; i++) {
+    list.push(i);
+  }
+@@@/script>
+<SvelScrollbar maxHeight={400} always>
+  {#each list as item}
+    <p class="scrollbar-demo-item">{item}</p>
+  {/each}
+</SvelScrollbar>
+`}
+>
+  <SvelScrollbar maxHeight={400}>
+    <div class="scrollbar-flex-content">
+      {#each list2 as item}
+        <p class="scrollbar-demo-item2">{item}</p>
+      {/each}
+    </div>
+  </SvelScrollbar>
+</Example>
+
 <style>
   .scrollbar-demo-item {
     display: flex;
@@ -50,5 +82,22 @@
     border-radius: 4px;
     background: var(--svel-color-primary-light-9);
     color: var(--svel-color-primary);
+  }
+
+  .scrollbar-flex-content {
+    display: flex;
+  }
+  .scrollbar-demo-item2 {
+    flex-shrink: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100px;
+    height: 50px;
+    margin: 10px;
+    text-align: center;
+    border-radius: 4px;
+    background: var(--svel-color-danger-light-9);
+    color: var(--svel-color-danger);
   }
 </style>
