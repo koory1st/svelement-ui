5ed8f54449ecaeaab7eeb2f74b8015037ae7fba0
koory1st <32436334@qq.com>
2023-12-13 20:21:18 +0800

    fix(slider): update

--- packages/slider/src/lib/button.svelte
+++ packages/slider/src/lib/button.svelte
@@ -2,6 +2,13 @@
   import a2s from '@svelement-ui/util-array-2-class-string';
   import { string as toStyleObject } from 'to-style';
 
+  export let value = null;
+  /** @type {number | null} */
+  export let precision = null;
+  export let max = Infinity;
+  export let min = -Infinity;
+  /** @type {number | null} */
+  export let step = 1;
   export let vertical = false;
   let button;
   $: hovering = false;
@@ -17,7 +24,6 @@
 
   $: disabled = false;
   $: isClick = false;
-  $: isClick = false;
   $: startY = 0;
   $: startX = 0;
   $: startPosition = 0;
@@ -25,6 +31,8 @@
   $: newPosition = 0;
   $: currentY = 0;
   $: currentX = 0;
+  $: sliderSize = 1;
+  $: oldValue = 0;
 
   function handleMouseEnter() {
     hovering = true;
@@ -40,46 +48,108 @@
 
   function onButtonDown(event) {
     if (disabled) return;
-    // onDragStart(event);
-    // window.addEventListener('mousemove', onDragging);
-    // window.addEventListener('touchmove', onDragging);
-    // window.addEventListener('mouseup', onDragEnd);
-    // window.addEventListener('touchend', onDragEnd);
-    // window.addEventListener('contextmenu', onDragEnd);
+    onDragStart(event);
+    window.addEventListener('mousemove', onDragging);
+    window.addEventListener('touchmove', onDragging);
+    window.addEventListener('mouseup', onDragEnd);
+    window.addEventListener('touchend', onDragEnd);
+    window.addEventListener('contextmenu', onDragEnd);
     button.focus();
   }
 
-  // function onDragStart(event) {
-  //   dragging = true;
-  //   isClick = true;
-  //   const { clientX, clientY } = getClientXY(event);
-  //   if (vertical) {
-  //     startY = clientY;
-  //   } else {
-  //     startX = clientX;
-  //   }
-  //   startPosition = Number.parseFloat(currentPosition.value);
-  //   newPosition = startPosition;
-  // }
-  //
-  // function onDragging(event) {
-  //   if (dragging) {
-  //     isClick = false;
-  //     displayTooltip();
-  //     resetSize();
-  //     let diff;
-  //     const { clientX, clientY } = getClientXY(event);
-  //     if (vertical) {
-  //       currentY = clientY;
-  //       diff = ((startY - currentY) / sliderSize) * 100;
-  //     } else {
-  //       currentX = clientX;
-  //       diff = ((currentX - startX) / sliderSize) * 100;
-  //     }
-  //     newPosition = startPosition + diff;
-  //     setPosition(newPosition);
-  //   }
-  // }
+  function onDragStart(event) {
+    dragging = true;
+    isClick = true;
+    const { clientX, clientY } = getClientXY(event);
+    if (vertical) {
+      startY = clientY;
+    } else {
+      startX = clientX;
+    }
+    startPosition = Number.parseFloat(currentPosition);
+    newPosition = startPosition;
+  }
+
+  function onDragging(event) {
+    if (dragging) {
+      isClick = false;
+      // displayTooltip();
+      // resetSize();
+      let diff;
+      const { clientX, clientY } = getClientXY(event);
+      if (vertical) {
+        currentY = clientY;
+        diff = ((startY - currentY) / sliderSize) * 100;
+      } else {
+        currentX = clientX;
+        diff = ((currentX - startX) / sliderSize) * 100;
+      }
+      newPosition = startPosition + diff;
+      setPosition(newPosition);
+    }
+  }
+
+  function getClientXY(event) {
+    let clientX;
+    let clientY;
+    if (event.type.startsWith('touch')) {
+      clientY = event.touches[0].clientY;
+      clientX = event.touches[0].clientX;
+    } else {
+      clientY = event.clientY;
+      clientX = event.clientX;
+    }
+    return {
+      clientX,
+      clientY,
+    };
+  }
+
+  async function setPosition(newPosition) {
+    if (newPosition === null || Number.isNaN(+newPosition)) return;
+    if (newPosition < 0) {
+      newPosition = 0;
+    } else if (newPosition > 100) {
+      newPosition = 100;
+    }
+    const lengthPerStep = 100 / ((max - min) / step);
+    const steps = Math.round(newPosition / lengthPerStep);
+    let v = steps * lengthPerStep * (max - min) * 0.01 + min;
+    v = Number.parseFloat(v.toFixed(precision));
+
+    if (v !== value) {
+      value = v;
+    }
+
+    if (!dragging && value !== oldValue) {
+      oldValue = value;
+    }
+
+    // await nextTick();
+    // dragging && displayTooltip();
+    // tooltip.value!.updatePopper();
+  }
+
+  function onDragEnd() {
+    if (!dragging) {
+      return;
+    }
+    setTimeout(() => {
+      dragging = false;
+      if (!hovering) {
+        // hideTooltip();
+      }
+      if (!isClick) {
+        setPosition(newPosition);
+      }
+      // emitChange();
+    }, 0);
+    window.removeEventListener('mousemove', onDragging);
+    window.removeEventListener('touchmove', onDragging);
+    window.removeEventListener('mouseup', onDragEnd);
+    window.removeEventListener('touchend', onDragEnd);
+    window.removeEventListener('contextmenu', onDragEnd);
+  }
 </script>
 
 <div
