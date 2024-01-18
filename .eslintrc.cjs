d6468d1521c28b171ff45985b8da8188219dbae3
koory1st <32436334@qq.com>
2024-01-18 18:47:14 +0800

    build: fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,30 @@
 module.exports = {
-    root: true,
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:svelte/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2020,
-        extraFileExtensions: ['.svelte'],
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  ignorePatterns: ['node_modules', '**/build', '**/dist', '**/.svelte-kit', '**/pnpm-lock.yaml'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
     },
-    env: {
-        browser: true,
-        es2017: true,
-        node: true,
-    },
-    ignorePatterns: ["node_modules", "**/build", "**/dist", "**/.svelte-kit", "**/pnpm-lock.yaml"],
-    overrides: [
-        {
-            files: ['*.svelte'],
-            parser: 'svelte-eslint-parser',
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-            },
-        },
-    ],
+  ],
 };
