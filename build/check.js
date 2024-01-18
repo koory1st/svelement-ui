b3d1283171e1793e52807e425b52f1459611f659
koory1st <32436334@qq.com>
2024-01-18 18:43:12 +0800

    build(check): add

---
+++ build/check.js
@@ -0,0 +1,17 @@
+const {execSync} = require('child_process');
+const lernaOutput = execSync('pnpm lerna-changed --json --toposort ', {encoding: 'utf-8'}).toString();
+const lines = lernaOutput.split('\n');
+const remainingLines = lines.slice(3);
+const outputString = remainingLines.join('\n');
+const packages = JSON.parse(outputString);
+
+const filterString = packages.map(element => {
+    console.log("check scope:", element.name)
+    return `--filter=${element.name}`
+// npm -r --filter=@svelement-ui/tooltip --filter=@svelement-ui/popper run check
+}).join(' ');
+console.log(filterString)
+const checkResult = execSync(`pnpm -r ${filterString} run check`).toString();
+console.log(checkResult)
+const lintResult = execSync(`pnpm -r ${filterString} run lint`).toString();
+console.log(lintResult)
