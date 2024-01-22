f853630101f8650c2040d9afd35230ee88e5569b
koory1st <32436334@qq.com>
2024-01-22 19:49:01 +0800

    fix(docs): add tooltip

--- docs/src/lib/i18n/zh/tooltip.js
+++ docs/src/lib/i18n/zh/tooltip.js
@@ -6,4 +6,7 @@ export default {
     '在这里我们提供 9 种不同方向的展示方式，可以通过以下完整示例来理解，选择你要的效果。',
   ctooltip02030:
     '使用 content 属性来决定 hover 时的提示信息。 由 placement 属性决定展示效果： placement属性值为：[方向]-[对齐位置]；四个方向：top、left、right、bottom；三种对齐位置：start, end，默认为空。 如 placement="left-end"，则提示信息出现在目标元素的左侧，且提示信息的底部与目标元素的底部对齐。',
+  ctooltip03010: '主题',
+  ctooltip03020: 'Tooltip 组件内置了两个主题：dark和light。',
+  ctooltip03030: '通过设置 effect 来修改主题，默认值为 dark.',
 };
--- docs/src/routes/component/tooltip/+page.svelte
+++ docs/src/routes/component/tooltip/+page.svelte
@@ -14,7 +14,127 @@
 <p>{$langFn('ctooltip02030')}</p>
 <Example
   code={`
+<div class="tooltip-base-box">
+  <div class="row center">
+    <SvelTooltip
+      class="box-item"
+      content="Top Left prompts info"
+      effect="dark"
+      placement="top-start"
+    >
+      <SvelButton>top-start</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip class="box-item" content="Top Center prompts info" effect="dark" placement="top">
+      <SvelButton>top</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip
+      class="box-item"
+      content="Top Right prompts info"
+      effect="dark"
+      placement="top-end"
+    >
+      <SvelButton>top-end</SvelButton>
+    </SvelTooltip>
+  </div>
+  <div class="row">
+    <SvelTooltip
+      class="box-item"
+      content="Left Top prompts info"
+      effect="dark"
+      placement="left-start"
+    >
+      <SvelButton>left-start</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip
+      class="box-item"
+      content="Right Top prompts info"
+      effect="dark"
+      placement="right-start"
+    >
+      <SvelButton>right-start</SvelButton>
+    </SvelTooltip>
+  </div>
+  <div class="row">
+    <SvelTooltip
+      class="box-item"
+      content="Left Center prompts info"
+      effect="dark"
+      placement="left"
+    >
+      <SvelButton>left</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip
+      class="box-item"
+      content="Right Center prompts info"
+      effect="dark"
+      placement="right"
+    >
+      <SvelButton>right</SvelButton>
+    </SvelTooltip>
+  </div>
+  <div class="row">
+    <SvelTooltip
+      class="box-item"
+      content="Left Bottom prompts info"
+      effect="dark"
+      placement="left-end"
+    >
+      <SvelButton>left-end</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip
+      class="box-item"
+      content="Right Bottom prompts info"
+      effect="dark"
+      placement="right-end"
+    >
+      <SvelButton>right-end</SvelButton>
+    </SvelTooltip>
+  </div>
+  <div class="row center">
+    <SvelTooltip
+      class="box-item"
+      content="Bottom Left prompts info"
+      effect="dark"
+      placement="bottom-start"
+    >
+      <SvelButton>bottom-start</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip
+      class="box-item"
+      content="Bottom Center prompts info"
+      effect="dark"
+      placement="bottom"
+    >
+      <SvelButton>bottom</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip
+      class="box-item"
+      content="Bottom Right prompts info"
+      effect="dark"
+      placement="bottom-end"
+    >
+      <SvelButton>bottom-end</SvelButton>
+    </SvelTooltip>
+  </div>
+</div>
 
+<style>
+.tooltip-base-box {
+  width: 600px;
+}
+.tooltip-base-box .row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.tooltip-base-box .center {
+  justify-content: center;
+}
+.tooltip-base-box .box-item {
+  width: 110px;
+  margin-top: 10px;
+}
+</style>
 `}
 >
   <div class="tooltip-base-box">
@@ -48,6 +168,125 @@
       >
         <SvelButton>left-start</SvelButton>
       </SvelTooltip>
+      <SvelTooltip
+        class="box-item"
+        content="Right Top prompts info"
+        effect="dark"
+        placement="right-start"
+      >
+        <SvelButton>right-start</SvelButton>
+      </SvelTooltip>
+    </div>
+    <div class="row">
+      <SvelTooltip
+        class="box-item"
+        content="Left Center prompts info"
+        effect="dark"
+        placement="left"
+      >
+        <SvelButton>left</SvelButton>
+      </SvelTooltip>
+      <SvelTooltip
+        class="box-item"
+        content="Right Center prompts info"
+        effect="dark"
+        placement="right"
+      >
+        <SvelButton>right</SvelButton>
+      </SvelTooltip>
+    </div>
+    <div class="row">
+      <SvelTooltip
+        class="box-item"
+        content="Left Bottom prompts info"
+        effect="dark"
+        placement="left-end"
+      >
+        <SvelButton>left-end</SvelButton>
+      </SvelTooltip>
+      <SvelTooltip
+        class="box-item"
+        content="Right Bottom prompts info"
+        effect="dark"
+        placement="right-end"
+      >
+        <SvelButton>right-end</SvelButton>
+      </SvelTooltip>
+    </div>
+    <div class="row center">
+      <SvelTooltip
+        class="box-item"
+        content="Bottom Left prompts info"
+        effect="dark"
+        placement="bottom-start"
+      >
+        <SvelButton>bottom-start</SvelButton>
+      </SvelTooltip>
+      <SvelTooltip
+        class="box-item"
+        content="Bottom Center prompts info"
+        effect="dark"
+        placement="bottom"
+      >
+        <SvelButton>bottom</SvelButton>
+      </SvelTooltip>
+      <SvelTooltip
+        class="box-item"
+        content="Bottom Right prompts info"
+        effect="dark"
+        placement="bottom-end"
+      >
+        <SvelButton>bottom-end</SvelButton>
+      </SvelTooltip>
+    </div>
+  </div>
+</Example>
+
+<h2>{$langFn('ctooltip03010')}</h2>
+<p>{$langFn('ctooltip03020')}</p>
+<p>{$langFn('ctooltip03030')}</p>
+
+<Example
+  code={`
+<div class="tooltip-base-box">
+  <div class="row">
+    <SvelTooltip content="Top center" placement="top">
+      <SvelButton>Dark</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip content="Bottom center" effect="light" placement="bottom">
+      <SvelButton>Light</SvelButton>
+    </SvelTooltip>
+    <SvelTooltip content="Bottom center" effect="customized">
+      <SvelButton>Customized theme</SvelButton>
+    </SvelTooltip>
+  </div>
+</div>
+
+<style>
+  :global(.svel-popper.is-customized) {
+    /* Set padding to ensure the height is 32px */
+    padding: 6px 12px;
+    background: linear-gradient(90deg, rgb(159, 229, 151), rgb(204, 229, 129));
+  }
+
+  :global(.svel-popper.is-customized .svel-popper__arrow::before) {
+    background: linear-gradient(45deg, #b2e68d, #bce689);
+    right: 0;
+  }
+</style>
+`}
+>
+  <div class="tooltip-base-box">
+    <div class="row">
+      <SvelTooltip content="Top center" placement="top">
+        <SvelButton>Dark</SvelButton>
+      </SvelTooltip>
+      <SvelTooltip content="Bottom center" effect="light" placement="bottom">
+        <SvelButton>Light</SvelButton>
+      </SvelTooltip>
+      <SvelTooltip content="Bottom center" effect="customized">
+        <SvelButton>Customized theme</SvelButton>
+      </SvelTooltip>
     </div>
   </div>
 </Example>
@@ -70,5 +309,16 @@
   :global(.tooltip-base-box .box-item) {
     width: 110px;
     margin-top: 10px;
+  }
+
+  :global(.svel-popper.is-customized) {
+    /* Set padding to ensure the height is 32px */
+    padding: 6px 12px;
+    background: linear-gradient(90deg, rgb(159, 229, 151), rgb(204, 229, 129));
+  }
+
+  :global(.svel-popper.is-customized .svel-popper__arrow::before) {
+    background: linear-gradient(45deg, #b2e68d, #bce689);
+    right: 0;
   }
 </style>
