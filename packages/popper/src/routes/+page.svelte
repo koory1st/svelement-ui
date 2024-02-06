a3536545df2514a6fc92a002bbdae8d6e10eeaff
koory1st <32436334@qq.com>
2024-02-06 19:37:46 +0800

    style(popper): add ignore

--- packages/popper/src/routes/+page.svelte
+++ packages/popper/src/routes/+page.svelte
@@ -6,7 +6,9 @@
 
 <div class="aaa">
   <div class="bbb" />
+  <!-- svelte-ignore a11y-mouse-events-have-key-events -->
   <button bind:this={ref} on:mouseover={(e) => (ref = e.currentTarget)}>11111</button>
+  <!-- svelte-ignore a11y-mouse-events-have-key-events -->
   <button bind:this={ref} on:mouseover={(e) => (ref = e.currentTarget)}>22222</button>
   <N virtualRef={ref} virtualTriggering />
 
